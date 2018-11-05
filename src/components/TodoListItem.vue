774bf2447e26fc4f482c381ce705934800933464
Byron Ferguson <ferguson.bs@gmail.com>
2018-11-05 13:29:39 -0600

    completed working todo list

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <label :class="{'completed': todo.completed}">
+      <input type="checkbox" v-model="todo.completed" />
+      {{ todo.task }}
+      <button @click="deleteTodo(todo)">Delete</button>
+    </label>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
